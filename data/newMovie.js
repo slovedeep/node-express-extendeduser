47e871d5599c6eefdcc2f5b616cf9ad765d76dcc
rgarciamvm <91730364+rgarciamvm@users.noreply.github.com>
2022-03-28 17:25:00 +0200

    Add files via upload

---
+++ data/newMovie.js
@@ -0,0 +1,33 @@
+export const new_movie = {
+    id: 10,
+    title: "Dances with Wolves",
+    genres: [
+        "Adventure", "Western ", "Drama"
+    ],
+    year: 1990,
+    director: "Kevin Costner",
+    actors: [
+        "Kevin Costner",
+        "Mary McDonnell",
+        "Graham Greene",
+        "Rodney A. Grant"
+    ]
+};
+
+export const new_movie_update = {
+    id: 3,
+    title: "El padrino parte II",
+    genres: [
+        "Crime", "Drama"
+    ],
+    year: 1994,
+    director: "Frank Darabont",
+    actors: [
+        "Tim Robbins",
+        "Ricard el profe",
+        "Bob Gunton",
+        "William Sadler",
+        "Clancy Brown",
+        "Gil Bellows"
+    ]
+};
---
+++ data/user.js
@@ -0,0 +1,8 @@
+export default [
+    {
+    username:'peterparker@dominio.es',
+    password:'$2b$10$oBZSp9xQcnWrQZAKsIpuK.NzSNr8CFNE5SRv/mPmvjGKqzphJZ/He',
+    timestamp:0,
+    active: 1
+    }
+]
